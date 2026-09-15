706ecae2851387e724dca75b6a4de4f343c63893
2311004 <2311004@A30214.student.ncs.ac.jp>
2026-09-15 16:33:12 +0900

    初回コミット

---
+++ src/main/java/com/example/attacksimulator/AttacksimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.attacksimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttacksimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttacksimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/attacksimulator/AttacksimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attacksimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttacksimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
