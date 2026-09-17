56630ab22581cdb7a59896498bd927832fa25231
2311004 <2311004@A30214.student.ncs.ac.jp>
2026-09-17 10:46:22 +0900

    あ

--- src/main/java/com/example/attacksimulator/AttacksimulatorApplication.java
+++ src/main/java/com/example/attacksimulator/AttacksimulatorApplication.java
@@ -1,5 +1,7 @@
 package com.example.attacksimulator;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +10,11 @@ import service.AttackSimulatorService;
 @SpringBootApplication
 public class AttacksimulatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AttacksimulatorApplication.class, args);
-		AttackSimulatorService simulator = new AttackSimulatorService();
-		simulator.start();
-	}
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(AttacksimulatorApplication.class, args);
 
-	
+        AttackSimulatorService simulator = new AttackSimulatorService();
+        simulator.start();
+    }
+
 }
